d5b215c3d5dac16b8dbbd6fadf324b71526eab0f
Bart Veneman <bartveneman@gmail.com>
2025-10-13 15:21:07 +0200

    fix: do not mark invalid/non-primitive fontFamily tokens as fontFamily

--- src/destructure-font-family.test.ts
+++ src/destructure-font-family.test.ts
@@ -21,18 +21,18 @@ test('multiple font-family with spaces and quotes', () => {
 	expect.soft(destructure_font_family('"Arial Black", sans-serif')).toEqual(['Arial Black', 'sans-serif'])
 })
 
+test('with emoji name', () => {
+	expect.soft(destructure_font_family('💪')).toEqual(['💪'])
+})
+
 test('single var()', () => {
-	expect.soft(destructure_font_family('var(--family)')).toEqual(['var(--family)'])
+	expect.soft(destructure_font_family('var(--family)')).toBeUndefined()
 })
 
 test('multiple var()', () => {
-	expect.soft(destructure_font_family('var(--family), var(--family2)')).toEqual(['var(--family)', 'var(--family2)'])
+	expect.soft(destructure_font_family('var(--family), var(--family2)')).toBeUndefined()
 })
 
 test('var() with fallback', () => {
-	expect.soft(destructure_font_family('var(--family, Arial)')).toEqual(['var(--family, Arial)'])
-})
-
-test('with emoji name', () => {
-	expect.soft(destructure_font_family('💪')).toEqual(['💪'])
+	expect.soft(destructure_font_family('var(--family, Arial)')).toBeUndefined()
 })
