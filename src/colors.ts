5db13a5bba18d35745ebefd9fe6d655383088097
Bart Veneman <1536852+bartveneman@users.noreply.github.com>
2025-10-26 15:22:07 +0100

    Colorjsio 0.6.0 beta.1 (#40)
    * chore: upgrade colorjs.io to 0.6.0-beta.1
    
    * failed to register all spaces in 1 go, so add TODO

--- src/colors.ts
+++ src/colors.ts
@@ -1,7 +1,7 @@
 import { colorKeywords as color_keywords, cssKeywords as css_keywords } from '@projectwallace/css-analyzer'
 import { type ColorValue, type ColorSpace as tColorSpace } from './types.js'
 import {
-	parse,
+	tryColor,
 	ColorSpace,
 	XYZ_D65,
 	XYZ_D50,
@@ -28,6 +28,8 @@ import {
 } from 'colorjs.io/fn'
 
 // Register color spaces for parsing and converting
+// TODO: According to the changelog we should be able to import
+// and register all spaces in one go but it doesn't seem to work
 ColorSpace.register(sRGB) // Parses keywords and hex colors
 ColorSpace.register(XYZ_D65)
 ColorSpace.register(XYZ_D50)
@@ -72,17 +74,14 @@ export function color_to_token(color: string): ColorValue | null {
 		return null
 	}
 
-	try {
-		let parsed_color = parse(color)
-		let [component_a, component_b, component_c] = parsed_color.coords
+	let parsed_color = tryColor(color)
 
-		return {
-			colorSpace: parsed_color.spaceId as tColorSpace,
-			components: [component_a ?? 'none', component_b ?? 'none', component_c ?? 'none'],
-			alpha: parsed_color.alpha ?? 1,
-		}
-	} catch (error) {
-		// A catch for edge cases that we don't support yet.
-		return null
+	if (parsed_color === null) return null
+	let [component_a, component_b, component_c] = parsed_color.coords
+
+	return {
+		colorSpace: parsed_color.space.id as tColorSpace,
+		components: [component_a ?? 'none', component_b ?? 'none', component_c ?? 'none'],
+		alpha: parsed_color.alpha ?? 1,
 	}
 }
